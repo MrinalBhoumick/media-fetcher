1dfb9d87139b6b5df434f02264b5650cf404278b
Mrinal Bhoumick <78199545+MrinalBhoumick@users.noreply.github.com>
2024-04-05 02:40:42 +0530

    Delete update-schema.sh

--- update-schema.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Parse data.graphql and convert to AppSync schema SDL
-sdl=$(graphql-codegen --config codegen.yml)
-
-# Update AppSync schema using AWS CLI
-aws appsync update-schema --api-id oxqwkuucofbrvht3l2s4iskrte --definition "$sdl"
